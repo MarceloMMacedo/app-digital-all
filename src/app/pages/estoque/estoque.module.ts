2669fb3c9d61ab48a0fbca74a7a86ec59c7b71a9
MarceloMMacedo <marcelo_macedo01@hotmail.com>
2021-08-19 22:41:56 -0300

    list-produtos

--- src/app/pages/estoque/estoque.module.ts
+++ src/app/pages/estoque/estoque.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { EstoqueRoutingModule } from './estoque-routing.module';
+import { ListProdutosComponent } from './list-produtos/list-produtos.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ListProdutosComponent
+  ],
   imports: [
     CommonModule,
     EstoqueRoutingModule
